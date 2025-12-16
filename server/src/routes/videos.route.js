472dcec464af2f7fb05e912c35b844810246ddaf
YusufByn <yusuf.buyukaydin33@gmail.com>
2025-12-16 11:30:04 +0100

    changement du poids fichier

--- server/src/routes/videos.route.js
+++ server/src/routes/videos.route.js
@@ -7,12 +7,14 @@ const router = Router();
 
 
 // route pour créer une vidéo
+// dans uploadVideo on a single('video') car on upload une seule video
 router.post('/', uploadVideo.single('video'), createVideoController);
 // route pour récupérer toutes les vidéos
 router.get('/', getAllVideosController);
 // route pour récupérer une vidéo par son id
 router.get('/:id', getVideoByIdController);
 // route pour modifier une vidéo (avec ou sans nouveau fichier)
+// dans uploadVideo on a single('video') car on upload une seule video
 router.put('/:id', uploadVideo.single('video'), updateVideoController);
 // route pour supprimer une vidéo
 router.delete('/:id', deleteVideoController);
